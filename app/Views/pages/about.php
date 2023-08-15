81898e75bc162f73e0597e2a77ac61966b20ade4
username_anjastabuni <reblytabuni2004@gmail.com>
2023-08-15 16:25:54 +0700

    Belajar CI4

---
+++ app/Views/pages/about.php
@@ -0,0 +1,12 @@
+<?= $this->extends('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusantium totam iure culpa veritatis nemo obcaecati minus adipisci. Asperiores architecto animi, nihil autem sed corrupti suscipit nemo incidunt laborum tenetur?</p>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
